e5200e4be342c9fa4aafa0723b10b308f3c3ba0b
Biplob Talukdar <biplob.codes@gmail.com>
2026-03-04 00:53:40 +0600

    Add video metadata

---
+++ prisma/migrations/20260303185307_add_video_metadata/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Video" ADD COLUMN "duration" REAL;
+ALTER TABLE "Video" ADD COLUMN "resolution" TEXT;
+ALTER TABLE "Video" ADD COLUMN "size" REAL;
+ALTER TABLE "Video" ADD COLUMN "thumbnailPath" TEXT;
